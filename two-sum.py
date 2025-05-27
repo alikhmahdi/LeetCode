ce04f525280f970db199d04bb9420444a8419a5c
alikhmahdi <thealikhmahdi@gmail.com>
2025-05-27 16:00:21 +0330

    Update two-sum.py

--- two-sum.py
+++ two-sum.py
@@ -1,11 +1,11 @@
-# nums = input()
-# nums = nums.strip("[]")
-# nums = [int(x) for x in nums.split(',')]
-# target = int(input())
-# for index1 in range(len(nums)):
-#     for index2 in range(index1+1, len(nums)):
-#         if target == nums[index1]+nums[index2]:
-#             print([index1, index2])
+nums = input()
+nums = nums.strip("[]")
+nums = [int(x) for x in nums.split(',')]
+target = int(input())
+for index1 in range(len(nums)):
+    for index2 in range(index1+1, len(nums)):
+        if target == nums[index1]+nums[index2]:
+            print([index1, index2])
 
 
 
@@ -18,21 +18,21 @@ class Solution:
                 
                 
 
-###################################################################
-# nums = input()
-# nums = nums.strip("[]")
-# nums = [int(x) for x in nums.split(',')]
-# target = int(input())
-# for num1 in nums:
-#     for num2 in nums[nums.index(num1)+1::]:
-#         if target == num1+num2:
-#             print([nums.index(num1), nums.index(num2)])
+##################################################################
+nums = input()
+nums = nums.strip("[]")
+nums = [int(x) for x in nums.split(',')]
+target = int(input())
+for num1 in nums:
+    for num2 in nums[nums.index(num1)+1::]:
+        if target == num1+num2:
+            print([nums.index(num1), nums.index(num2)])
 
 
 
-# class Solution:
-#     def twoSum(self, nums: List[int], target: int) -> List[int]:
-#         for num1 in nums:
-#             for num2 in nums[nums.index(num1)+1::]:
-#                 if target == num1+num2:
-#                     return [nums.index(num1), nums.index(num2)]
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for num1 in nums:
+            for num2 in nums[nums.index(num1)+1::]:
+                if target == num1+num2:
+                    return [nums.index(num1), nums.index(num2)]
